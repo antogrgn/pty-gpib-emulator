975e0f0c95178d5b47d9fc6826c797bc67d49799
Aidan Macdonald <aidan.plenert.macdonald@gmail.com>
2014-01-04 15:51:53 -0800

    Added Documentation to each file       FakePrologix is now PTY_Interface.py

---
+++ PTY_Interface.py
@@ -0,0 +1,83 @@
+##
+## ****  PTY_Interface Class ****
+## by Aidan Macdonald (aidan.plenert.macdonald@gmail.com)
+## 
+## This class generates a pty device that emulates the
+## tty device created by GPIB controllers such as
+## Prologix GPIB-USB Controller (prologix.biz)
+## 
+## This class does NOT emulate the controller, but the
+## controller must be attached to this PTY Interface
+## 
+## I may switch between the terms tty device and pty device.
+## They refer to the same thing.
+##
+
+
+import os
+import Device
+
+class PTY_Interface:
+
+## Initialization - PTY_Interface(default_device, default_address)
+## accepts arguments default_device and default_address
+## the default_device will be connected at the default_address
+## 
+## If the Interface address (PTY_Interface.addr) is changed, the
+## no longer have anything attached and will become functionless.
+## UNLESS, the default device handles the address change and copies itself
+## to the new address (Prologix_GPIB_USB does this, OR there is
+## another device attached there.
+##
+## if no arguments are given at initialization, defaults are
+## Device.Prologix_GPIB-USB at address 0
+##
+    def __init__(self, default_device=Device.Prologix_GPIB_USB, default_address=0):
+        self.m, self.s = os.openpty()
+        self.devices = {default_address:default_device}
+        self.addr = default_address
+
+## printFilename()
+## Prints the path to the PTY device generated (ie /dev/pts/2)
+##
+    def printFilename(self):
+        print(os.ttyname(self.s))
+
+## run(readLen)
+## Runs the emulator which will read all statements written from the outside
+## to the pty device at the location given by self.printFilename(), then
+## runs what was read through self.handleInput(tty_in) and writes output
+## back to the pty device to be read from the outside
+##
+    def run(self, readLen=100):
+        self.running = True
+        while (self.running):
+            tty_in = os.read(self.m, readLen)
+            for tty_out in self.handleInput(tty_in):
+                os.write(self.m, tty_out)
+
+
+## handleInput(tty_in)
+## Recieves raw input that was read from the pty device, and 
+## breaks it up and send it to the device listed at the current
+## address. That device must handle the command via dev.handleCMD(cmd, args)
+##
+    def handleInput(self, tty_in):
+        tty_in = tty_in.strip()
+
+        if tty_in != "" and self.addr in self.devices:
+            dev = self.devices[self.addr]
+            for cmd in tty_in.strip().split(";"):
+                p = cmd.strip().split(" ")
+                yield dev.handleCMD(self, p[0], p[1:]) + "\n"
+
+        yield ""  # Must be a null string to prevent infinite loop
+
+## addDevice(device, address)
+## Preferred way of connecting a device to the interface
+## at a given address
+##
+    def addDevice(self, dev, addr):
+        self.devices[addr] = dev
+        
+
---
+++ Prologix.py
@@ -0,0 +1,85 @@
+##
+## **** Prologix_GPIB_USB Class ****
+##
+## A Device that emulates the functionality of the
+## Prologix GPIB-USB Controller (prologix.biz)
+## 
+
+
+import Device, PTY_Interface
+
+class Prologix_GPIB_USB(Device):
+
+## Initialization - Prologix_GPIB_USB()
+## Does not take arguments, but is VERY important
+## All classes that attempt to inherit this class,
+## must call this initialization.
+##
+## This creates a dictionary containing all the commands
+## to be run. If this dictionary is not created, this class
+## does nothing
+##
+    def __init__(self):
+        self.cmds = { "++ver":self.version, # "COMMAND":HANDLER
+                      "++addr":self.addr,
+                      "++kill":self.kill}
+
+## handleCMD(pty_interface, command, arguments)
+## See Device.handleCMD documentation
+##
+## In Prologix_GPIB_USB, this function makes use of the
+## map made in initialization to find the command and
+## its associated handler, then call the handler and pass in
+## the pty_interface and the arguments
+##
+    def handleCMD(self, fpl, cmd, args=[]):
+        out = Device.handleCMD(self, fpl, cmd, args)
+        if out != "":
+            return out
+        elif cmd in self.cmds:
+            return self.cmds[cmd](fpl, args)
+        else:
+            return ""
+
+## version(pty, args)
+## Returns the device version
+##
+    def version(self, fpl, args=[]):
+        return "Prologix Version 1.1 Simulator"
+
+## addr(pty, args)
+## If no arguments are given, returns the current address
+## of the pty interface
+##
+## if args are given, then an integer is parsed out of
+## the first argument and set as the address. This ensures
+## that a device exists at the following address
+##
+## Any errors return "ERROR"
+##
+    def addr(self, fpl, args=[]):
+        if args == []:
+            return str(fpl.addr)
+        else:
+            try:
+                fpl.addr = int(args[0])
+                if not fpl.addr in fpl.devices:
+                    fpl.addDevice(PrologixAdapter(), fpl.addr)
+                return ""
+            except:
+                return "ERROR"
+## kill(pty, args)
+## Stops the pty interface from running
+##     
+    def kill(self, fpl, args=[]):
+        fpl.running = False
+        return ""
+
+## addCMD(command, handler_function)
+## Adds a command to the dictionary
+## Handler function must be of the form
+## func(pty_interface, arguments):
+##
+    def addCMD(self, cmd, func):
+        self.cmds[cmd] = func
+
